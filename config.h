47d5efcd3aef15d5b335cbad43a41f9d7df8d68f
Jensun Ravichandran <jjensun@gmail.com>
2020-11-17 13:18:25 +0100

    Customize fonts

--- config.h
+++ config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+
+static char *font = "Ubuntu Mono Nerd Font-13:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
